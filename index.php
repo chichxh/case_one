78f1d57d19438cbecae48e5db31971a293e54bee
доена <69032321+husduy@users.noreply.github.com>
2022-03-30 22:48:33 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ямщицкое подворье</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Jost&family=Montserrat&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<!-- Хидер -->
+	<nav class="navbar navbar-light navbar-expand-lg bg-light font-mnt">
+	  <div class="container">
+	    <a class="navbar-brand" href="#">Ямщицкое подворье</a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+	      <div class="offcanvas-header">
+	        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Ямщицкое подворье</h5>
+	        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+	      </div>
+	      <div class="offcanvas-body">
+	        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+	          <li class="nav-item">
+	            <a class="nav-link active" aria-current="page" href="#">Главная</a>
+	          </li>
+	          <li class="nav-item">
+	            <a class="nav-link" href="#">Новости</a>
+	          </li>
+	          <li class="nav-item">
+	            <a class="nav-link" href="#">Мерч</a>
+	          </li>
+	          <li class="nav-item">
+	            <a class="nav-link" href="#">Музеи</a>
+	          </li>
+	          <li class="nav-item">
+	            <a class="nav-link" href="#">Контакты</a>
+	          </li>
+	        </ul>
+	      </div>
+	    </div>
+	  </div>
+	</nav>
+	<!-- Карта -->
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col-12">
+				<h1 class="font-mnt"><b>История ямщиков у тебя в кармане</b></h1>
+				<p class="font-jt">Интерактивная карта и много возможностей для изучения жизни и истории ямщиков</p>
+			</div>
+		</div>
+	</div>
+	<div class="container mt-5">
+		<div class="row">
+			
+		</div>
+	</div>
+
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+</html>
