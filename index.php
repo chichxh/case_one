4e8156ba0cffda4e224412ef976df81ac7628d31
{доена} <egorodand@gmail.com>
2022-03-31 00:31:40 +0900

    add

--- index.php
+++ index.php
@@ -13,37 +13,37 @@
 <body>
 	<!-- Хидер -->
 	<nav class="navbar navbar-light navbar-expand-lg bg-light font-mnt">
-	  <div class="container">
-	    <a class="navbar-brand" href="#">Ямщицкия подворья</a>
-	    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-	      <span class="navbar-toggler-icon"></span>
-	    </button>
-	    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-	      <div class="offcanvas-header">
-	        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Ямщицкие подворья</h5>
-	        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-	      </div>
-	      <div class="offcanvas-body">
-	        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-	          <li class="nav-item">
-	            <a class="nav-link active" aria-current="page" href="#">Главная</a>
-	          </li>
-	          <li class="nav-item">
-	            <a class="nav-link" href="#">Новости</a>
-	          </li>
-	          <li class="nav-item">
-	            <a class="nav-link" href="#">Мерч</a>
-	          </li>
-	          <li class="nav-item">
-	            <a class="nav-link" href="#">Музеи</a>
-	          </li>
-	          <li class="nav-item">
-	            <a class="nav-link" href="#">Контакты</a>
-	          </li>
-	        </ul>
-	      </div>
-	    </div>
-	  </div>
+		<div class="container">
+		    <a class="navbar-brand" href="index.php"><img src="img/logo.png" width="120"></a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+		        <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+			    <div class="offcanvas-header">
+			        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src="img/logo.png" width="120"></h5>
+			        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+			    </div>
+			    <div class="offcanvas-body">
+			        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+				        <li class="nav-item">
+				            <a class="nav-link active" aria-current="page" href="#">Главная</a>
+				        </li>
+				        <li class="nav-item">
+				            <a class="nav-link" href="#">Новости</a>
+				        </li>
+				        <li class="nav-item">
+				            <a class="nav-link" href="#">Мерч</a>
+				        </li>
+				        <li class="nav-item">
+				            <a class="nav-link" href="#">Музеи</a>
+				        </li>
+				        <li class="nav-item">
+				            <a class="nav-link" href="#">Контакты</a>
+				        </li>
+			        </ul>
+			    </div>
+		    </div>
+		</div>
 	</nav>
 	<!-- Карта -->
 	<div class="container mt-5">
@@ -53,6 +53,43 @@
 				<p class="font-jt">Интерактивная карта и много возможностей для изучения жизни и истории ямщиков</p>
 			</div>
 		</div>
+
+		<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+			<div class="carousel-indicators">
+			    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+			</div>
+			<div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <div class="carousel-caption d-none d-md-block">
+			        <h5>First slide label</h5>
+			        <p>Some representative placeholder content for the first slide.</p>
+			      </div>
+			    </div>
+			    <div class="carousel-item">
+			      <div class="carousel-caption d-none d-md-block">
+			        <h5>Second slide label</h5>
+			        <p>Some representative placeholder content for the second slide.</p>
+			      </div>
+			    </div>
+			    <div class="carousel-item">
+			      <div class="carousel-caption d-none d-md-block">
+			        <h5>Third slide label</h5>
+			        <p>Some representative placeholder content for the third slide.</p>
+			      </div>
+			    </div>
+			</div>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="visually-hidden">Next</span>
+			</button>
+		</div>
+
 		<div class="row mt-5">
 			<h1 class="font-mnt"><b>Карта</b></h1>
 			<p class="font-jt">Узнайте где и когда</p>
@@ -72,14 +109,32 @@
 			<p class="font-jt">Наши возможности</p>
 		</div>
 		<div class="row justify-content-between">
-			<div class="col-3">
-				<p>Лошадиная ферма</p>
+			<div class="col-4">
+				<div class="card">
+					<div class="card-body">
+					    <h5 class="card-title">Лошадиная ферма</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn">Узнать больше</a>
+					</div>
+				</div>
 			</div>
-			<div class="col-3">
-				<p>Туртреки</p>
+			<div class="col-4">
+				<div class="card">
+					<div class="card-body">
+					    <h5 class="card-title">Туртрекер</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn">Узнать больше</a>
+					</div>
+				</div>
 			</div>
-			<div class="col-3">
-				<p>Аренда кафе</p>
+			<div class="col-4">
+				<div class="card">
+					<div class="card-body">
+					    <h5 class="card-title">Аренда кафе</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn">Узнать больше</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
